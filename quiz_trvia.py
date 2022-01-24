350400ceb58c2a9c9d3de6ed7c830fda54e90d78
Dan Smestad <hockeydan25@gmail.com>
2022-01-23 21:58:37 -0600

    Project1_Capstone. file for quiz trivia program. Work to : improve the code, and make it more concise, flexible, structured, readable, and user-friendly, with a focus on code quality.

---
+++ quiz_trvia.py
@@ -0,0 +1,96 @@
+"""Project1 Quiz Program Dan Smestad ITEC 2905-80 Software Dev. Capstone Clara James.
+version one
+ A quiz program. written for student editing """
+
+#data structue Dicionary to hold data that can be interchanged. May have to rethink the my list names. 
+question_bank = {"art": {"questions": 
+["Who painted the Mona Lisa?","What precious stone is used to make the artist\'s pigment ultramarine?",
+"Anish Kapoor\'s bean-shaped Cloud Gate scuplture is a landmark of which city?"],
+"correct_answers" : ["Leonardo Da Vinci","Lapiz lazuli","Chicago"]},
+"Space": {"questions":
+["Which planet is closest to the sun?", 
+"Which planet spins in the opposite direction to all the others in the solar system?",
+"How many moons does Mars have?"],
+"correct_answers": ["Mercury" ,"Venus","two"]}}
+
+def main():
+    try:     
+        total_score = 0   #count the score of coorect answers.
+        users_answer = [] #collect the users acutal inputs.
+        #question_bank["hockey"] = "what do the letters NHL stand? "#add new pair to existing dictionary.
+        print('Welcome to my Triva Quiz program!Topic options are: ')
+        for k,v in question_bank.items(): # reads the question bank key names and print for user to select one.
+            print(k)
+        selected_for_topic = input('Please choose a topic, what is your trvia selection? ')# simple instructions to start.
+        
+        if selected_for_topic in question_bank.keys(): # vaildating that they select one of the choices.                     
+            print(f'{selected_for_topic} is your choice')# simple instructions to help show users what is happening.
+            question_bank_list = list(question_bank.values())
+
+            #creating art question list to loop over, use for loop. Maybe this could be more generic name/
+            art_question_list = [question_bank_list[0]["questions"][0], 
+            question_bank_list[0]["questions"][1],
+            question_bank_list[0]["questions"][2]] 
+
+            #creating art question answer list to loop over, use for loop.Maybe this could be more generic name/
+            art_question_answer_list = [question_bank_list[0]["correct_answers"][0], 
+            question_bank_list[0]["correct_answers"][1],
+            question_bank_list[0]["correct_answers"][2]]
+
+            for question in art_question_list: #for loop loops through let without len function.
+                print(question)    #variable question will start at 0 count of list.         
+                users_answer = input('enter your answer here: ')  #if users_answer in art_question_answer_list:                           
+                if  users_answer in art_question_answer_list:                
+                    print(f'that was correct, for each correct answer you earn one point,'
+                    ' your next question will load now.') #point of referance to user
+                    total_score += 1   #score total incrementing
+                else:
+                    print(f'Oops, sorry that was incorrect answer.')
+                    #was working on loop here to give correct answer for each loop but keep getting all three answers.
+            print('End of quiz!')# simple note that quiz has ended. total good byt message follows
+            print(f'Your total score on {selected_for_topic} questions is {total_score} out of 3.')
+             # extra statement to congratulate user on a well played game, possible to give extra points if all 3 correct.    
+            if total_score == 3:
+                print('You got all the answers correct!') 
+        else: #starting spce triva. Haven't added additional Hockey category questions noteed about.
+            print(f'You selected space for your triva quesions.')
+            question_bank_list = list(question_bank.values())
+                #art_questions = question_bank_list[0]["questions"][0]
+                # print(art_questions)
+            space_question_list = [question_bank_list[1]["questions"][0], 
+            question_bank_list[1]["questions"][1],
+            question_bank_list[1]["questions"][2]] 
+
+            space_question_answer_list = [question_bank_list[1]["correct_answers"][0], 
+            question_bank_list[1]["correct_answers"][1],
+            question_bank_list[1]["correct_answers"][2]]
+           
+            for question in space_question_list: #for loop loops through let without len function.
+                print(question) 
+                users_answer = input('enter your answer here: ')  
+                    #if users_answer in art_question_answer_list:         
+                if users_answer in space_question_answer_list:                                 
+                    print(f'that was correct, for each correct answer you earn one point,'
+                    'your next question will load now or game will end.') 
+                    total_score += 1 # point total socre increments one through loop                    
+                else: 
+                    print(f'Oops, sorry that was incorrect answer.') #incorrect statement to user.
+                    # needed to wotk out giving correct answer here possible.
+            print('End of quiz!') #closing note
+            print(f'Your total score on {selected_for_topic} questions is {total_score} out of 3.')
+            # extra statement to congratulate user on a well played game, possible to give extra points if all 3 correct.
+            if total_score == 3:
+                print('You got all the answers correct!')        
+    except:     #validation error handling for 
+            print('Opps we didn\'t get the question loaded please pause.')  
+            #possible error for service outage.     
+       
+main() # call main to start /or restart program.
+
+# print(f'Your sletion {selection_for_topic} is not a choice, please try agian, select from the list.')
+
+            # if selection_for_topic not in question_bank.keys():
+            #     print(f'Your seletion {selection_for_topic} is not a choice, please try agian, select from the list.')
+            #     print('OK, let\'s play Trvia, make a new slection')
+            #     restart = main()         #will return to start with list to select.
+            # else: 
