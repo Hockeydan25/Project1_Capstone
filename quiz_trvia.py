f2e4c3f894c6e0c335625e8f8af54b32d0d3dff8
Dan Smestad <hockeydan25@gmail.com>
2022-01-26 11:30:30 -0600

    fixed rework on upper case lower case, fixed the ouputs varible and counter.

--- quiz_trvia.py
+++ quiz_trvia.py
@@ -38,8 +38,8 @@ question_bank = {
 }
 
 def main():
-    try:
-        score = 0
+    #try:
+        #score = 0
         print('Welcome to my Triva Quiz program!Topic options are: ')                         
         for k,v in question_bank.items(): # reads the question bank key names and print for user to select one.
             print(k)
@@ -47,18 +47,18 @@ def main():
         get_input(selected_topic)# simple instructions to start.
         questions_for_topic = question_bank[selected_topic] # list of questions.
         quiz_answers = question_bank[selected_topic] # list of correct answers
-        get_quiz(questions_for_topic, quiz_answers)
+        score = get_quiz(questions_for_topic, quiz_answers)
         outputs(score)
         #outputs(selected_topic) 
-    except Exception as e:
-      print(e) 
+    # except Exception as e:
+    #   print('error',e) 
         
 def get_input(selected_topic):  
     if selected_topic not in question_bank.keys(): # vaildating that they select one of the choices.                     
         restart()
     else:
-        print(f'{selected_topic} is your choice')# simple instructions to help show users what is happening.
-        
+        print(f'{selected_topic} is your choice')
+        # simple instructions to help show users selection is being used.        
     return selected_topic   
 
 def get_quiz(questions_for_topic, quiz_answers):# argumants needed to setup questions and answers.
@@ -68,34 +68,38 @@ def get_quiz(questions_for_topic, quiz_answers):# argumants needed to setup ques
     score = 0  #initalize score for tracking users total score. set to zero at start of quiz.
     for question, correct_answer in zip(quiz_questions, quiz_answers):        
         print(question) 
-                    # todo ask user question 
-        users_answer = input('enter your answer here: ')
-        if users_answer.lower() == correct_answer.lower():   
-            score +=1
-            print(f'correct, {score} point added.')        
-        else:
+        #asking user question, to answer the question. 
+        users_answer = input('enter your answer here: ') #variable and input function to prompt top user pregroam ready for there input.
+        if users_answer.lower()  == correct_answer.lower(): #need to accept any case and validate correct answer.   
+            score +=1  #point for correct answer. #TO FIX : validation correct_answer.
+            print(f'correct, {score} point added.')#staement that qustio  was answered with correct answer.         
+        else:                          # maybe better with score total call to total_scotre().
             print(f'Oops, sorry that was incorrect answer.')
+            # To FIX: remove this commentedOUT section once output is working.
     # print('End of quiz!')# simple note that quiz has ended. total good byt message follows
     # print(f'Your total score in Quiz Triva is {score} out of 3.')
     # if score == 3:
     #         print('You got all the answers correct!')
     # extra statement to congratulate user on a well played game, 
     # possible to give extra points if all 3 correct
+    print(score)
     return score
-def outputs(score):
-    score = 0
+
+    #todo:  fix outputs get understanind of local variable usage here and counter not working.
+def outputs(score):  
+    
     print('End of quiz!')# simple note that quiz has ended. total good byt message follows
     print(f'Your total score in Quiz Triva is {score} out of 3.')
     if score == 3:
             print('You got all the answers correct! See you next time')
     # extra statement to congratulate user on a well played game, 
     # possible to give extra points if all 3 correct
-    
-
+    return score
+    # to fix users incorrect topic is appearing at end of preogram. Also need a break    
 def restart():    #requested User input to play again. gets used when topic other than the ones listed is entered by the user.
-    restart = input('Your entery was not in the list of Topics. Would you like to select one from the list? Enter y or n: ') 
-    if restart.lower() == 'y':
-        main()
+    retry = input('Your entery was not in the list of Topics. Would you like to select one from the list? Enter y or n: ') 
+    if retry.lower() == 'y':
+        main()  
     else:
         print('thanks for playing!') #good bye message               
 
