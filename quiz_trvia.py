9d6f3132a0ee04990bb51393e4074b4eac6ff333
Dan Smestad <hockeydan25@gmail.com>
2022-01-26 20:37:07 -0600

    added final notes, modified outputs to add selected topic, modifed the print score note to used in get_quiz and output. Removed try exept error handlin from main.

--- quiz_trvia.py
+++ quiz_trvia.py
@@ -13,7 +13,7 @@
 9.	Pay attention to code quality and style. Descriptive variable and function names, write efficient, logical, readable code.
  """
 
-#data structue Dicionary to hold data that can be interchanged. May have to rethink the my list names. 
+#data structure, Dicionary to hold data retieved and used to run program. readability with { [ , spacing. 
 question_bank = {   
 "art": 
         {   
@@ -48,25 +48,22 @@ question_bank = {
 }
 
 def main():
-    #try:
-        #score = 0
+
         print('Welcome to my Triva Quiz program!Topic options are: ')                         
         for k,v in question_bank.items(): # reads the question bank key names and print for user to select one.
             print(k)        
         selected_topic= get_input()# simple instructions to start.
-        questions_for_topic = question_bank[selected_topic] # list of questions.
-        quiz_answers = question_bank[selected_topic] # list of correct answers
+        questions_for_topic = question_bank[selected_topic] # variable to hold list of questions from dictionary question_bank.
+        quiz_answers = question_bank[selected_topic] # variable to hold list of correct answers from dictinary question_bank.
         score = get_quiz(questions_for_topic, quiz_answers)
-        outputs(score)
-        #outputs(selected_topic) 
-    # except Exception as e:
-    #   print('error',e) 
+        outputs(score, selected_topic) 
         
 def get_input():  
     while True:
-        selected_topic = input('Please choose a trvia selection topic? ')# simple instructions to start.
-        
-        if selected_topic not in question_bank.keys(): # vaildating that they select one of the choices.                     
+        selected_topic = input('Please choose a trvia selection topic? ')
+        # simple instructions to start, user selects a triva topic.
+        # vaildating that they select one of the choices. will keep in While loop until uset types topic in list.        
+        if selected_topic not in question_bank.keys():                      
             print('that was not in our choices') 
         else:
             print(f'{selected_topic} is your choice')
@@ -75,32 +72,29 @@ def get_input():
 
 def get_quiz(questions_for_topic, quiz_answers):# argumants needed to setup questions and answers.
     #variables set to use an loop through questions answers. localized function variables. 
-    quiz_questions = questions_for_topic['questions']  # list of question strings
-    quiz_answers = questions_for_topic['correct_answers']  # list of correct answer
+    quiz_questions = questions_for_topic['questions']  # variables list of question strings.
+    quiz_answers = questions_for_topic['correct_answers']  # variables list of correct answer.
     score = 0  #initalize score for tracking users total score. set to zero at start of quiz.
     for question, correct_answer in zip(quiz_questions, quiz_answers):        
         print(question) 
         #asking user question, to answer the question. 
         users_answer = input('enter your answer here: ') #variable and input function to prompt top user pregroam ready for there input.
         if users_answer.lower()  == correct_answer.lower(): #need to accept any case and validate correct answer.   
-            score +=1  #point for correct answer. #TO FIX : validation correct_answer.
-            print(f'correct, {score} point added.')#staement that qustion  was answered with correct answer.         
-        else:                        # maybe better with score total call to total_scotre().
-            print(f'Oops, sorry that was incorrect answer.')
-            # To FIX: remove this commentedOUT section once output is working.
+            score +=1  #point for correct answer.
+            print(f'correct, current points earned = {score}.')#staement that qustion  was answered with correct answer.         
+        else:      #message that answer is incorrect, points earned given too.
+            print(f'Oops, sorry that was incorrect answer, current points earned = {score}.')
+            # could have a retry here to give user another try or add a hint to user to get correct anwser.
+    return score # returns score to main and outputs will use.
 
-    print(score)
-    return score
-
-    #todo:  fix outputs get understanind of local variable usage here and counter not working.
-def outputs(score):      
+    #outputs dispplays end messaging to user with score and goodbye.
+def outputs(score, selected_topic):      
     print('End of quiz!')# simple note that quiz has ended. total good byt message follows
-    print(f'Your total score in Quiz Triva is {score} out of 3.')
+    print(f'Your total score today in Quiz Triva is {score} out of 3.')
     if score == 3:
-            print('You got all the answers correct! See you next time!')
+            print(f'You got all the Quiz Triva answers on {selected_topic} Triva correct! See you next time!')
     # extra statement to congratulate user on a well played game, 
     # possible to give extra points if all 3 correct
-    return score
+    return score, selected_topic
              
 main() # call main to start /or restart program.    
-
